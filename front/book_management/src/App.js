d56af4ddb0f85e7662fe9ec3e25560b0adde41f6
otso <hhgg0017@gmail.com>
2023-04-19 11:46:59 +0900

    2023. 04. 19. callback 함수, react Promise

--- front/book_management/src/App.js
+++ front/book_management/src/App.js
@@ -3,6 +3,8 @@ import { Reset } from "./styles/Global/reset";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import Callback from "./study/Callback";
+import PromiseStudys from "./study/PromiseStudys";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <Global styles={Reset}></Global>
       <Routes>
         <Route exact path="/login" Component={Login} />
-        <Route path="register" Component={Register} />
+        <Route path="/register" Component={Register} />
+        <Route path="/callback" Component={Callback} />
+        <Route path="/promise" Component={PromiseStudys} />
       </Routes>
     </>
   );
