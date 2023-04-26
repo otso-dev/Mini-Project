af910f145070752211c8b1c93a32ba5c13dbe878
otso <hhgg0017@gmail.com>
2023-04-26 12:26:01 +0900

    update

--- front/book_management/src/pages/BookDetail/BookDetail.js
+++ front/book_management/src/pages/BookDetail/BookDetail.js
@@ -4,6 +4,7 @@ import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
+import axios from "axios";
 
 const mainContainer = css`
   padding: 10px;
@@ -11,23 +12,38 @@ const mainContainer = css`
 
 const BookDetail = () => {
   const { bookId } = useParams();
-  const getBook = useQuery(["getBook"], async () => {});
-  return (
-    <div css={mainContainer}>
-      <Sidebar />
-      <header>
-        <h1></h1>
-        <p>분류: /저자명: / 출판사: / 추천:</p>
-      </header>
-      <main>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div></div>
-        <div></div>
-      </main>
-    </div>
-  );
+  const getBook = useQuery(["getBook"], async () => {
+    const option = {
+      headers: {
+        Authorization: localStorage.getItem("accessToken"),
+      },
+    };
+    const response = await axios.get(`http://localhost:8080/book/${bookId}`, option);
+    return response;
+  });
+
+  if (getBook.isLoading) {
+    return <div>로딩중...</div>;
+  }
+  if (!getBook.isLoading)
+    return (
+      <div css={mainContainer}>
+        <Sidebar />
+        <header>
+          <h1>{getBook.data.data.bookName}</h1>
+          <p>
+            분류: {getBook.data.data.categoryName}/저자명: {getBook.data.data.authorName}/ 출판사: {getBook.data.data.publisherName}/ 추천: 10
+          </p>
+        </header>
+        <main>
+          <div>
+            <img src={getBook.data.data.coverImgUrl} alt={getBook.data.data.bookName} />
+          </div>
+          <div></div>
+          <div></div>
+        </main>
+      </div>
+    );
 };
 
 export default BookDetail;
