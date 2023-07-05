7f7a67be4caee7fa1a3ad928295f5e618da003f3
otso <hhgg0017@gmail.com>
2023-07-05 15:50:43 +0900

    feat: socket 채팅 수정

--- SocketProject_Chatting/SocketServer/src/Server/Server/ServerApplication.java
+++ SocketProject_Chatting/SocketServer/src/Server/Server/ServerApplication.java
@@ -1,4 +1,5 @@
-package Server;
+package Server.Server;
+
 
 import java.io.IOException;
 import java.net.ConnectException;
--- SocketProject_Chatting/SocketServer/src/Server/Server/SocketServer.java
+++ SocketProject_Chatting/SocketServer/src/Server/Server/SocketServer.java
@@ -1,4 +1,4 @@
-package Server;
+package Server.Server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
